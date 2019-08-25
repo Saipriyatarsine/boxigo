eb8d2f12cf44c6e049b789698a71fe9a632ed2d3
Jeevan:M <csejeevan@gmail.com>
2019-08-25 18:52:24 +0530

    admin dashboard

--- objects/Admin_dashboard.php
+++ objects/Admin_dashboard.php
@@ -2,46 +2,25 @@
 class Admin_Dashboard{
     // database connection and table name
     private $conn;
-    private $Customer_table_name = "customers";
-    private $Vendor_table_name = "vendor";
-    private $Vendor_Request_table_name = "vendor_request";
-
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
-    }
-    
-
- 
+    } 
     // select "customers" as Type, count(*) as total from customers union all
     // Customer count function
     function Customer_count(){
-        $query = "SELECT 'customers' as Type, COUNT(*) as total  FROM " . $this->Customer_table_name ;
-     
+        $query = "SELECT 'customers' as Type, count(*) as total from customers
+        union all
+        SELECT 'vendor_request' as Type,count(*) as total from vendor_request
+        union all
+        SELECT 'vendor' as Type, count(*) as total from vendor  ";
+
+        echo $query;
+        
         $stmt = $this->conn->prepare( $query );
         $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row;
+        return $stmt;
     }
 
-    // Vendor count function
-    function Vendor_count(){
-        $query = "SELECT 'Vendor' as Type, COUNT(*) as total  FROM " . $this->Vendor_table_name ;
-     
-        $stmt = $this->conn->prepare( $query );
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row;
-    }
-
-    // Vendor_Request count function 
-    function Vendor_Request_count(){
-        $query = "SELECT 'Vendor_Request' as Type, COUNT(*) as total  FROM " . $this->Vendor_Request_table_name ;
-     
-        $stmt = $this->conn->prepare( $query );
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row;
-    }
 }
 ?>
---
+++ product/customer_count.php
@@ -0,0 +1,36 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../objects/Admin_dashboard.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$Customer_count = new Admin_Dashboard($db);
+
+$stmt = $Customer_count->Customer_count();
+
+
+if(!empty($stmt)){
+    $products_arr=array();
+    $products_arr["Total Customer_counts"]=array($stmt->fetch(PDO::FETCH_ASSOC));
+     http_response_code(200);
+     echo json_encode($products_arr);
+}
+else{
+    http_response_code(404);
+     echo json_encode(
+        array("message" => "No services found.")
+    );
+}
+
+
+
+
+?>
